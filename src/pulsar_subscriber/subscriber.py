b243af45e55819b1469b0affc0c36b867c1ad6ca
Parham Alvani <parham.alvani@gmail.com>
2025-12-04 08:20:04 +0000

    add python subscriber

---
+++ src/pulsar_subscriber/subscriber.py
@@ -0,0 +1,17 @@
+import asyncio
+from nats.aio.client import Client as NatsClient
+
+async def subscribe(address, subject):
+    nc = NatsClient()
+
+    await nc.connect(address)
+
+    async def message_handler(msg):
+        print(f"Received on [{msg.subject}]: {msg.data.decode()}")
+
+    # Subscribe to your topic
+    await nc.subscribe(subject=subject, cb=message_handler)
+
+    print("Listening on {0}...".format(subject))
+    while True:
+        await asyncio.sleep(1)
